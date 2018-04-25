b6e2c9862413db3e6903836cd4c7569c08679776
edere <edere@Rene-Laptop>
2018-04-25 14:56:06 +0200

    asasiufhsj

--- src/gui/ListPanel.java
+++ src/gui/ListPanel.java
@@ -8,23 +8,18 @@ import javax.swing.table.DefaultTableModel;
 
 import bll.Task;
 
-public class ListPanel extends JPanel {
+public class ListPanel extends JTable {
 
 	private String[] titles =null;
 	private DefaultTableModel model = null;
-	private JTable table = null;
 	
 	public ListPanel() {
-		this.setLayout(new BorderLayout ());
 		initializeControls();
 		this.setVisible(true);	
 	}
 	
 	public void initializeControls() {
 		this.titles = new String[]{ "Kategorie", "Fach", "Beschreibung", "Von","Bis","Erledigt" };
-		this.model = new DefaultTableModel( titles, 0 );
-		this.table = new JTable( model );
-		this.add(table, BorderLayout.CENTER);
-		
+		this.model.setColumnIdentifiers(titles);
 	}
 }
