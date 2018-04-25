3d52b14bb587850992dc899a7c8f3549c8566d43
edere <edere@Rene-Laptop>
2018-04-25 18:56:35 +0200

    Table funktioniert!

--- src/gui/ListPanel.java
+++ src/gui/ListPanel.java
@@ -1,28 +1,36 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.Vector;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import bll.Task;
 
-public class ListPanel extends JTable {
+public class ListPanel extends JPanel  {
 
-	private String[] titles =null;
+	private String[] titles = null;
 	private DefaultTableModel model = null;
-	
+	private JScrollPane scroll = null;
+	private JTable table =null;
+
 	public ListPanel() {
 		initializeControls();
-		this.setVisible(true);	
+		this.setVisible(true);
 	}
-	
+
 	public void initializeControls() {
+		this.titles = new String[] { "Kategorie", "Fach", "Beschreibung", "Von", "Bis", "Erledigt" };
+
+		this.model = new DefaultTableModel(titles,0); 
 		
-		this.model= new DefaultTableModel();
-		this.setModel(model);
-		this.titles = new String[]{ "Kategorie", "Fach", "Beschreibung", "Von","Bis","Erledigt" };
-		this.model.setColumnIdentifiers(titles);
+		this.table =new JTable(model);
+		this.scroll = new JScrollPane(this.table);
+		
+		this.setLayout(new BorderLayout());
+		this.add(this.scroll, BorderLayout.CENTER);
 	}
 }
