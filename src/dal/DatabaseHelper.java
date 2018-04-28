f5e06e9821fff17e34fb84d99201dd34e107b8c4
Philipp <Philipp@Philipp>
2018-04-28 14:35:48 +0200

    gfgfgfdg

--- src/dal/DatabaseHelper.java
+++ src/dal/DatabaseHelper.java
@@ -3,8 +3,8 @@ package dal;
 import java.sql.*;
 import java.util.ArrayList;
 
+import bll.Kategorie;
 import bll.Task;
-
 
 public class DatabaseHelper {
 	public static ArrayList<Task> loadData() {
@@ -13,15 +13,15 @@ public class DatabaseHelper {
 		ResultSet rs = null;
 		Task s = null;
 		ArrayList<Task> studentarray=new ArrayList<Task>();
-		java.util.Date datum=new java.util.Date();
+		java.util.Date von=null;
+		java.util.Date bis=null;
 
-		
 		try {
 
 			/* Step 1 Registrieren des Treibers */
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			/* Step 2 */
-			con = DriverManager.getConnection("jdbc:oracle:thin:d3b06/d3b@192.168.128.152:1521:ora11g");
+			con = DriverManager.getConnection("jdbc:oracle:thin:d3b06/d3b@212.152.179.117:1521:ora11g");
 			/* Step 3 */
 			stmt_Select = con.createStatement();
 			/* Step 4 */
@@ -29,7 +29,43 @@ public class DatabaseHelper {
 
 			while (rs.next()) {
 				/* Step 5 */
-			//	s = new Task(rs.getString(1), rs.getString(2), rs.getString(3), datum.parse(rs.getDate(4).toString()), rs.getDate(5), rs.getString(6));
+				Kategorie kategorie=null;
+				
+				if(rs.getString(1)=="Hausübung") {
+					kategorie=Kategorie.Hausübung;
+				}
+				
+				else if(rs.getString(1)=="Schularbeit") {
+					kategorie=Kategorie.Schularbeit;	
+				}
+				
+				else if(rs.getString(1)=="Schulübung") {
+					kategorie=Kategorie.Schulübung;
+				}
+				
+				else if(rs.getString(1)=="PLF") {
+					kategorie=Kategorie.PLF;
+				}
+				
+				else if(rs.getString(1)=="Mitarbeitskontrolle") {
+					kategorie=Kategorie.Mitarbeitskontrolle;
+				}
+				
+				else if(rs.getString(1)=="GLF") {
+					kategorie=Kategorie.GLF;
+				}
+				
+				von=rs.getDate(4);
+				bis=rs.getDate(5);
+				
+				if(rs.getString(6)=="true") {
+					s = new Task(kategorie, rs.getString(2), rs.getString(3), von, bis, true);
+				}
+				
+				else if(rs.getString(6)=="false") {
+					s = new Task(kategorie, rs.getString(2), rs.getString(3), von, bis, false);
+				}
+				
 				studentarray.add(s);
 			}
 		} catch (ClassNotFoundException e) {
@@ -50,23 +86,23 @@ public class DatabaseHelper {
 		return studentarray;
 	}
 
-	public static void saveData(ArrayList<Task> studentarray) {
+	public static void saveData(ArrayList<Task> taskarray) {
 		Connection con = null;
 		Statement stmt_Insert = null;
 		ResultSet rs = null;
-
+		//nfl
 		try {
 			/* Step 1 Registrieren des Treibers */
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			/* Step 2 */
-			con = DriverManager.getConnection("jdbc:oracle:thin:d3b06/d3b@192.168.128.152:1521:ora11g");
+			con = DriverManager.getConnection("jdbc:oracle:thin:d3b06/d3b@212.152.179.117:1521:ora11g");
 
 			/* Step 3 */
 			stmt_Insert = con.createStatement();
 			/* Step 4 */
 			
-			for(Task s : studentarray) {
-	//			rs = stmt_Insert.executeQuery("INSERT INTO schueler VALUES(" + s.getKatalognummer() + ", '" + s.getVorname() +"', '" + s.getNachname() +"')");
+			for(Task t : taskarray) {
+				rs = stmt_Insert.executeQuery("INSERT INTO schueler VALUES(" + t.getKategorie() + ", '" + t.getFach() +"', '" + t.getBeschreibung() + "', '" + t.getVon() + "', '" + t.getBis() + "', '" + t.getIsDone() +"')");
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
